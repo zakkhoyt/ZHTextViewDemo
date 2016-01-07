2799c9f1c5058b9f22e2de376502913a298cc2fa
Zakk Hoyt <vaporwarewolf@gmail.com>
2016-01-06 17:46:16 -0800

    bold and italic working independently, but not together

--- ZHTextViewDemo/ZHViewController.m
+++ ZHTextViewDemo/ZHViewController.m
@@ -25,63 +25,67 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 // SIngle ' = italic
 // double '' = bold
 
 - (void)textViewDidChange:(UITextView *)textView {
     NSDictionary *boldAttr = @{NSFontAttributeName: [UIFont boldSystemFontOfSize:textView.font.pointSize]};
-    NSAttributedString *boldText = [self formatText:textView.text betweenToken:@"''" attributes:boldAttr];
+    NSAttributedString *boldText = [self formatAttributedText:textView.attributedText betweenToken:@"''" attributes:boldAttr];
     if(boldText) {
         textView.attributedText = boldText;
-    } else {
-        NSDictionary *italicAttr = @{NSFontAttributeName: [UIFont italicSystemFontOfSize:textView.font.pointSize]};
-        NSAttributedString *italicText = [self formatText:textView.text betweenToken:@"'" attributes:italicAttr];
-        if(italicText) {
-            textView.attributedText = italicText;
-        }
+    }
+
+    NSDictionary *italicAttr = @{NSFontAttributeName: [UIFont italicSystemFontOfSize:textView.font.pointSize]};
+    NSAttributedString *italicText = [self formatAttributedText:textView.attributedText betweenToken:@"'" attributes:italicAttr];
+    if(italicText) {
+        textView.attributedText = italicText;
     }
 }
 
 
--(NSAttributedString*)formatText:(NSString*)text betweenToken:(NSString*)token attributes:(NSDictionary*)attributes {
+-(NSAttributedString*)formatAttributedText:(NSAttributedString*)attributedText betweenToken:(NSString*)token attributes:(NSDictionary*)attributes {
     NSError *error = nil;
     NSString *pattern = [NSString stringWithFormat:@"\%@([^\%@]*?)\%@", token, token, token];
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern
-                                                                               options:NSRegularExpressionCaseInsensitive
-                                                                                 error:&error];
-
-    NSRange fullRange = NSMakeRange(0, text.length);
+                                                                           options:NSRegularExpressionCaseInsensitive
+                                                                             error:&error];
     
-    NSUInteger boldMatchCount = [regex numberOfMatchesInString:text options:NSMatchingReportCompletion range:fullRange];
+    NSRange fullRange = NSMakeRange(0, attributedText.length);
+    
+    NSUInteger boldMatchCount = [regex numberOfMatchesInString:attributedText.string options:NSMatchingReportCompletion range:fullRange];
     
     if(boldMatchCount > 0) {
         NSLog(@"%lu matches", (unsigned long)boldMatchCount);
         
-        NSTextCheckingResult *result = [regex firstMatchInString:text options:NSMatchingReportCompletion range:fullRange];
+        NSTextCheckingResult *result = [regex firstMatchInString:attributedText.string options:NSMatchingReportCompletion range:fullRange];
         if(result) {
             NSRange fullRange = result.range;
-            NSString *fullSubStr = [text substringWithRange:fullRange];
+            NSString *fullSubStr = [attributedText.string substringWithRange:fullRange];
             NSLog(@"fullSubStr: %@", fullSubStr);
             
             NSRange prettyRange = NSMakeRange(fullRange.location + 1*token.length, fullRange.length - 2*token.length);
-            NSString *prettySubStr = [text substringWithRange:prettyRange];
+            NSString *prettySubStr = [attributedText.string substringWithRange:prettyRange];
             NSLog(@"prettySubStr: %@", prettySubStr );
+            
+            NSAttributedString *prettyAttrString = [attributedText attributedSubstringFromRange:prettyRange];
+            NSLog(@"prettyAttrString.string: %@", prettyAttrString.string );
             
             if(prettyRange.length == 0) {
                 return nil;
             }
             
-            NSMutableString *newString = [[NSMutableString alloc]initWithString:text];
+            NSMutableString *newString = [[NSMutableString alloc]initWithString:attributedText.string];
             [newString replaceCharactersInRange:fullRange withString:prettySubStr];
+            
             
             NSRange attrRange = NSMakeRange(fullRange.location, fullRange.length - 2*token.length);
             
@@ -90,14 +94,61 @@
             
             return newAttrString;
         } else {
-            NSLog(@"Error");
+            NSLog(@"Error applying regex: %@", error.localizedDescription);
         }
-    } else {
-        NSLog(@"No matches");
     }
-
+    
     return nil;
 }
+
+
+
+//-(NSAttributedString*)formatText:(NSString*)text betweenToken:(NSString*)token attributes:(NSDictionary*)attributes {
+//    NSError *error = nil;
+//    NSString *pattern = [NSString stringWithFormat:@"\%@([^\%@]*?)\%@", token, token, token];
+//    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern
+//                                                                               options:NSRegularExpressionCaseInsensitive
+//                                                                                 error:&error];
+//
+//    NSRange fullRange = NSMakeRange(0, text.length);
+//
+//    NSUInteger boldMatchCount = [regex numberOfMatchesInString:text options:NSMatchingReportCompletion range:fullRange];
+//
+//    if(boldMatchCount > 0) {
+//        NSLog(@"%lu matches", (unsigned long)boldMatchCount);
+//
+//        NSTextCheckingResult *result = [regex firstMatchInString:text options:NSMatchingReportCompletion range:fullRange];
+//        if(result) {
+//            NSRange fullRange = result.range;
+//            NSString *fullSubStr = [text substringWithRange:fullRange];
+//            NSLog(@"fullSubStr: %@", fullSubStr);
+//
+//            NSRange prettyRange = NSMakeRange(fullRange.location + 1*token.length, fullRange.length - 2*token.length);
+//            NSString *prettySubStr = [text substringWithRange:prettyRange];
+//            NSLog(@"prettySubStr: %@", prettySubStr );
+//
+//            if(prettyRange.length == 0) {
+//                return nil;
+//            }
+//
+//            NSMutableString *newString = [[NSMutableString alloc]initWithString:text];
+//            [newString replaceCharactersInRange:fullRange withString:prettySubStr];
+//
+//            NSRange attrRange = NSMakeRange(fullRange.location, fullRange.length - 2*token.length);
+//
+//            NSMutableAttributedString *newAttrString = [[NSMutableAttributedString alloc]initWithString:newString];
+//            [newAttrString addAttributes:attributes range:attrRange];
+//
+//            return newAttrString;
+//        } else {
+//            NSLog(@"Error");
+//        }
+//    } else {
+//        NSLog(@"No matches");
+//    }
+//
+//    return nil;
+//}
 
 
 @end
